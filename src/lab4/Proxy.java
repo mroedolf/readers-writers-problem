6e1c5d5aba66b3677bfcb16acad0cbfdc2aab08d
Maarten Roedolf <mroedolf@hotmail.com>
2022-12-02 14:30:36 +0100

    Added mutual exclusion

--- src/lab4/Proxy.java
+++ src/lab4/Proxy.java
@@ -1,13 +1,17 @@
 package lab4;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Proxy
 {
+    private ReentrantLock mutex = new ReentrantLock();
+
     private static Proxy instance = null;
 
 
     private Proxy()
     {
-        
+
     }
 
     public static synchronized Proxy getInstance()
@@ -19,21 +23,33 @@ public class Proxy
 
     public void preRead(int pid)
     {
+        try {
+            mutex.lock();
+        } finally {
+
+        }
         // TODO: synchronization before read goes here
     }
 
     public void postRead(int pid)
     {
+        mutex.unlock();
         // TODO: synchronization after read goes here
     }
 
     public void preWrite(int pid)
     {
+        try {
+            mutex.lock();
+        } finally {
+
+        }
         // TODO: synchronization before write goes here
     }
 
     public void postWrite(int pid)
     {
+        mutex.unlock();
         // TODO: synchronization after write goes here
     }
 }
